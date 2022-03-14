3306523a6a2eb89ff4d85f461da6eabf4c8346b7
ACEGAZ <garylowles@gmail.com>
2022-03-14 10:27:29 +0000

    add code to generate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
-function runGame() {
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "submit") {
+            alert("you clicked submit!");
+        } else {
+            let gameType = this.getAttribute("data-type")
+            alert(`you clicked"${gameType}`);
+        }
+        })
+    }
+})
 
+function runGame() {
+    let num1 = math.floor(math.random() * 25) + 1;
+    let num2 = math.floor(math.random() * 25) + 1;
 }
 
 function checkAnswer() {
